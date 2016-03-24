e25a448377fe593e1469a7bd8c8cd6258b6c62e4
duanyangjing <gmjingdy@gmail.com>
2016-03-23 22:48:21 -0500

    Module 8 assignment completed

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function setMap() {
 		.center([0, 36.33])
 		.rotate([-104.45, 0, 0])
 		.parallels([29.5, 45.17])
-		.scale(817.55)
+		.scale(850)
 		.translate([width / 2, height / 2]);
 
 	var path = d3.geo.path()
@@ -23,10 +23,14 @@ function setMap() {
 		.defer(d3.csv, "data/population.csv")
 		// TODO: Try see if the original shapefile is projected
 		.defer(d3.json, "data/ChinaProvinces.topojson")
+		.defer(d3.json, "data/AsiaRegion_6simplified.topojson")
 		.await(callback); //send data to callback function once finish loading
 
-	function callback(error, pop, prov) {
+	function callback(error, pop, prov, asia) {
 		// second parameter need to inspect element and find objects
+		console.log(asia.objects);
+		console.log(prov.objects);
+		var asiaRegion = topojson.feature(asia, asia.objects.AsiaRegion);
 		var provinces = topojson.feature(prov, prov.objects.collection).features;
 
 		var attrArray = ["city_unmarried_m_f", "rural_unmarried_m_f", "city_baby_m_f", "rural_baby_m_f", "total_city_percent"];
@@ -52,6 +56,10 @@ function setMap() {
 		var graticule = d3.geo.graticule()
             .step([10, 10]); //place graticule lines every 10 degrees of longitude and latitude
 
+        var gratBackground = map.append("path")
+        	.datum(graticule.outline())
+        	.attr("class", "gratBackground")
+        	.attr("d", path);
 
         // create graticule lines  
         var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
@@ -61,6 +69,10 @@ function setMap() {
             .attr("class", "gratLines") //assign class for styling
             .attr("d", path); //project graticule lines
 
+        var backgroundCountry = map.append("path")
+        	.datum(asiaRegion)
+        	.attr("class", "backgroundCountry")
+        	.attr("d", path);
 
 		// select all must take a list, should be a list of features, not the whole feature collection
 		var enumUnits = map.selectAll(".provinces")

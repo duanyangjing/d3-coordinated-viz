18e134a52e4da845913ad2f6d9ad537d65bc980b
duanyangjing <gmjingdy@gmail.com>
2016-03-24 21:47:52 -0500

    Module 9 almost done, need more styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.onload = setMap();
 
 function setMap() {
-	var width = 960, height = 550;
+	var width = window.innerWidth * 0.5, height = 550;
 
 	var map = d3.select("body")
 		.append("svg")
@@ -39,8 +39,11 @@ function setMap() {
         	.attr("class", "backgroundCountry")
         	.attr("d", path);
 
-		setEnumUnits(provinces, map, path);
+        var colorScale = makeColorScale(pop);
+		
+		setEnumUnits(provinces, map, path, colorScale);
 
+		setChart(pop, colorScale);
 
 	};
 };
@@ -87,14 +90,132 @@ function joinData(provinces, pop) {
 	return provinces;
 };
 
-function setEnumUnits(provinces, map, path) {
+function setEnumUnits(provinces, map, path, colorScale) {
 		// select all must take a list, should be a list of features, not the whole feature collection
 	var enumUnits = map.selectAll(".provinces")
 		.data(provinces)
 		.enter()
 		.append("path")
-		.attr("class", function(d) {
+		.attr("class", "enumUnits")
+		.attr("id", function(d) {
 			return d.properties.name;
 		})
-		.attr("d", path);
+		.attr("d", path)
+		.style("fill", function(d){
+			return choropleth(d.properties, colorScale);
+		});
+};
+
+function makeColorScale(data) {
+	console.log(data);
+    var colorClasses = [
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
+    ];
+
+    var colorScale = d3.scale.threshold()
+    	.range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i = 0; i < data.length; i++){
+        var val = parseFloat(data[i]["city_baby_m_f"]);
+        domainArray.push(val);
+    };
+
+    //cluster data using ckmeans clustering algorithm to create natural breaks
+    var clusters = ss.ckmeans(domainArray, 5);
+    //reset domain array to cluster minimums
+    domainArray = clusters.map(function(d){
+        return d3.min(d);
+    });
+    //remove first value from domain array to create class breakpoints
+    domainArray.shift();
+
+    //assign array of last 4 cluster minimums as domain
+    colorScale.domain(domainArray);
+
+    return colorScale; 
+}
+
+// deal with enumUnits without data
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props["city_baby_m_f"]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (val && val != NaN){
+        return colorScale(val);
+    } else {
+        return "#CCC";
+    };
+};
+
+function setChart(pop, colorScale) {
+	var chartWidth = window.innerWidth * 0.425, chartHeight = 550;
+
+	var chart = d3.select("body")
+		.append("svg")
+		.attr("width", chartWidth)
+		.attr("height", chartHeight)
+		.attr("class", "chart");
+	// yscale to generate the height of each bar based on attribute value
+	var yScale = d3.scale.linear()
+		.range([0, chartHeight])
+		.domain([100, 140])
+
+	var bars = chart.selectAll(".bars")
+		.data(pop)
+		.enter()
+		.append("rect")
+		.sort(function(a, b){
+			return a["city_baby_m_f"] - b["city_baby_m_f"];
+		})
+		.attr("class", function(d){
+			return "bars " + d.name;
+		})
+		.attr("width", chartWidth / pop.length - 1)
+		.attr("x", function(d, i){
+			return i * (chartWidth / pop.length);
+		})
+		.attr("height", function(d){
+			return yScale(parseFloat(d["city_baby_m_f"]));
+		})
+		.attr("y", function(d){
+			return chartHeight - yScale(parseFloat(d["city_baby_m_f"]));
+		})
+		.style("fill", function(d){
+			return choropleth(d, colorScale);
+		})
+
+	// annotate each bar in the chart
+	var numbers = chart.selectAll(".numbers")
+		.data(pop)
+		.enter()
+		.append("text")
+		.sort(function(a, b){
+			return a["city_baby_m_f"] - b["city_baby_m_f"];
+		})
+		.attr("class", function(d){
+			return "numbers " + d.name;
+		})
+		.attr("text-anchor", "middle")
+		.attr("x", function(d, i){
+			var fraction = chartWidth / pop.length;
+			return i * fraction + (fraction - 1) / 2;
+		})
+		.attr("y", function(d){
+			return chartHeight - yScale(parseFloat(d["city_baby_m_f"])) + 15;
+		})
+		.text(function(d){
+			return d["city_baby_m_f"];
+		});
+
+	var chartTitle = chart.append("text")
+		.attr("x", 20)
+		.attr("y", 40)
+		.attr("class", "chartTitle")
+		.text("city_baby_m_f");
 };
